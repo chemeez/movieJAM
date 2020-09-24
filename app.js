1bce504a9a6bf63d220e5094500ef2e226a35036
junga <ja0643@naver.com>
2020-09-24 17:37:53 +0900

    MINMANGMUNG

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var expressLayouts = require('express-ejs-layouts');
 
 var indexRouter = require('./routes/index');
 
@@ -12,17 +13,17 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.set('layout', 'layout');
+app.set('layout extractScripts', true);
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(expressLayouts);
 
 app.use('/', indexRouter);
-
-
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
